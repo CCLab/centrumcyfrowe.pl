e94f0e354d710afcac14e383e24ee65fa388f05f
KasiaWerner <kwerner@centrumcyfrowe.pl>
2018-09-05 10:53:22 +0200

    posibility of adding Polish letters in landing page menu

--- modules/contents-item.php
+++ modules/contents-item.php
@@ -1,5 +1,9 @@
 <?php
     $section = get_sub_field('nazwa_sekcji');
-    $section_id = str_replace(' ', '-', $section);
+ 
+$polish = array('ą', 'ę', 'ć', 'ł', 'ń', 'ś', 'ó', 'ź', 'ż', ' ');
+$english  = array('a', 'e', 'c', 'l', 'n', 's', 'o', 'z', 'z','-');
+
+$section_id = str_ireplace($polish, $english, $section);
 ?>
 <span id="<?php echo $section_id ?>" data-section="<?php echo $section ?>" class="contents-item"></span>
