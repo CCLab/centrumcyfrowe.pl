0e2b69347de44974e600c0b9fdf76337a4ff0235
KasiaWerner <kwerner@centrumcyfrowe.pl>
2019-04-04 11:54:27 +0200

    Update 404.php

--- 404.php
+++ 404.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
+  <main class="tekstowa">
+    <div class="container">
+      <div class="text-center">
+        <h3>Szukana strona nie istnieje. Wróć do <a style="color:#ff5722;" href="https://centrumcyfrowe.pl/">strony głównej</a>. </h3>
 
+      </div>
+    </div>
+  </main>
 <?php get_footer(); ?>
